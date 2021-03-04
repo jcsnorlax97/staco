ec289563790a03d3ae7d361bb7d8baeb182d39fd
Justin <justin.choi.developer@gmail.com>
2021-03-04 00:57:32 -0600

    Prototype Search bar UI: - Add a form tag (prevent auto page refreshing; trigger searchStacoViaTag when the btn is clicked) - Add an input field for entering tag (v-model for binding; placeholder) - Add a label associated to the input field - Add a button for submitting the form - [DEV] Display the entered tag at real time - [DEV] Alert and console.log the entered tag when the form is submitted

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
-  <div id="id"></div>
+  <div id="id">
+    Staco
+    <br />
+    Search Bar
+    <form
+      action=""
+      class="search-bar__search-staco-via-tag"
+      @submit.prevent="searchStacoViaTag"
+    >
+      <label for="tag">Tag: </label>
+      <input type="text" id="tag" v-model="tag" placeholder="e.g. javascript" />
+      <button>Search!</button>
+    </form>
+    {{ tag }}
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      tag: '',
+    }
+  },
+  methods: {
+    searchStacoViaTag() {
+      if (this.tag && this.tag !== '') {
+        console.log(`Tag ${this.tag} has been entered!`)
+        alert(`Tag ${this.tag} has been entered!`)
+      }
+    },
+  },
 }
 </script>
 
