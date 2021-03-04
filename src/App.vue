71203363a128f94bd87907a984460a4d31750680
Justin <justin.choi.developer@gmail.com>
2021-03-04 01:29:13 -0600

    Refactor for SearchBar: - Move SearchBar into a single component file - Update App.vue (import SearchBar, mention it in components, add that component in template)

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,17 @@
 <template>
   <div id="id">
     Staco
-    <br />
-    Search Bar
-    <form
-      action=""
-      class="search-bar__search-staco-via-tag"
-      @submit.prevent="searchStacoViaTag"
-    >
-      <label for="tag">Tag: </label>
-      <input type="text" id="tag" v-model="tag" placeholder="e.g. javascript" />
-      <button>Search!</button>
-    </form>
-    {{ tag }}
+    <SearchBar />
   </div>
 </template>
 
 <script>
+import SearchBar from './components/SearchBar.vue'
+
 export default {
   name: 'App',
-  data() {
-    return {
-      tag: '',
-    }
-  },
-  methods: {
-    searchStacoViaTag() {
-      if (this.tag && this.tag !== '') {
-        console.log(`Tag ${this.tag} has been entered!`)
-        alert(`Tag ${this.tag} has been entered!`)
-      }
-    },
+  components: {
+    SearchBar,
   },
 }
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="search-bar">
+    Search Bar
+    <form
+      action=""
+      class="search-bar__search-staco-via-tag"
+      @submit.prevent="searchStacoViaTag"
+    >
+      <label for="tag">Tag: </label>
+      <input type="text" id="tag" v-model="tag" placeholder="e.g. javascript" />
+      <button>Search!</button>
+    </form>
+    {{ tag }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data() {
+    return {
+      tag: '',
+    }
+  },
+  methods: {
+    searchStacoViaTag() {
+      if (this.tag && this.tag !== '') {
+        console.log(`Tag ${this.tag} has been entered!`)
+        alert(`Tag ${this.tag} has been entered!`)
+      }
+    },
+  },
+}
+</script>
